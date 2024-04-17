a1fd8490682322a9783a476880f0ce7a6f4b5f8d
linzili <1983160303@qq.com>
2024-04-17 23:03:56 +0800

    1

--- satoken/src/main/kotlin/com/satoken/controller/EnumController.kt
+++ satoken/src/main/kotlin/com/satoken/controller/EnumController.kt
@@ -2,6 +2,7 @@ package com.satoken.controller
 
 import com.satoken.dto.TestDto
 import com.satoken.enums.GenderEnum
+import com.satoken.enums.SourceEnum
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -12,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("enum")
 class EnumController {
 
-    @PostMapping
-    fun getGender(@RequestBody gender: TestDto) = gender.genderEnum
+//    @PostMapping
+//    fun getGender(@RequestBody gender: TestDto) = gender.genderEnum
+
+
+    @PostMapping("test")
+    fun test(@RequestBody source:TestDto) = source
 }
--- satoken/src/main/kotlin/com/satoken/dto/TestDto.kt
+++ satoken/src/main/kotlin/com/satoken/dto/TestDto.kt
@@ -1,7 +1,10 @@
 package com.satoken.dto
 
 import com.satoken.enums.GenderEnum
+import com.satoken.enums.SourceEnum
+import io.swagger.v3.oas.annotations.media.Schema
 
+@Schema(description = "测试DTO")
 data class TestDto(
-    val genderEnum:GenderEnum
+    val sourceEnum: SourceEnum
 )
